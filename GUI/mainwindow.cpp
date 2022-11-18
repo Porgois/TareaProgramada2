6a005c2ba19fc3c993ba757a532a7323b781d254
Isaac Vargas <isaac.vargasjimenez20@ucr.ac.cr>
2022-11-17 19:11:13 -0600

    Arreglos GUI
    -Se agregaron elementos extra a la interfaz (marcos, cambio tipo de texto...etc.).
    -Se hizo que la imagen generada aparezca en el centro del marco.

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -20,32 +20,43 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_leer_clicked()
 {
+   //creates variables path and name for future use.
    QString Qpath = QInputDialog::getText(this, " ", "Enter path:");
    path = Qpath.toStdString();
 
    QString Qname = QInputDialog::getText(this, " ", "Enter name:");
    name = Qname.toStdString();
 
-   //sets full_name
+   //sets full_name.
    full_name = path + name + ".bmp";
 
-   //display the image read
+   //display the image read.
    QString url = full_name.c_str();
    QPixmap img(url);
-   ui->label->setPixmap(img);
+   ui->imagen->setPixmap(img);
+
+   //Align Image to center.
+   ui->imagen->setAlignment(Qt::AlignCenter);
 }
 
 void MainWindow::on_negative_clicked()
 {
-    //c++ function to interpret and edit image at path/name.bmp
+    //c++ function to interpret and edit image at path/name.bmp.
     read(path, name);
 
     //sets filter image to the edited one.
     edited_name = path + name + "_edited.bmp";
     std::cout << "edited name: " << edited_name << std::endl;
 
+    //displays the edited image.
     QString url1 = edited_name.c_str();
     QPixmap img1(url1);
-    ui->label_2->setPixmap(img1);
+    ui->imagen_editada->setPixmap(img1);
+
+    //Align Image to center.
+    ui->imagen_editada->setAlignment(Qt::AlignCenter);
+
 }
+
+//Ejemplo PATH: C:/Users/porgois/Desktop/ (nombre de archivo sin extension)
 
